d4eaffa09db27b2a1a0037bd09ba298b6cf6c7bf
erepie <pierre.mgouedard@gmail.com>
2019-02-28 11:12:47 +0100

    prod final facile 2

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# Install pip
-sudo apt update && sudo apt install python-pip && echo "python-pip installed" || echo "python-pip install failed"
-
 # Fork facile erp
 git clone https://github.com/pierreGouedard/facile.git && cd facile && "facile ERP forked" ||
 echo "fork of facile ERP failed"
+
+# Install pip
+sudo apt -q update && echo Y | sudo apt -q install python-pip && echo "python-pip installed" || echo "python-pip install failed"
 
 # Install virtualenv and create it
 sudo pip install virtualenv && virtualenv fvirt && source fvirt/bin/activate &&
 echo "virtualenv created and activated" || echo "virtualenv created and activated"
 
 # Install requirements
-pip install -r requirements.txt
+pip install -qr requirements.txt
+
+# Install Elastic Beanstalk Environment
+pip install -q awsebcli && echo "fvirt" > .ebignore && echo n | eb init -p python-2.7 facile-erp-0001 --region eu-west-3a
+
