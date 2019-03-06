4580067d7c24125beb511075c743b136163a4124
erepie <pierre.mgouedard@gmail.com>
2019-03-06 20:42:36 +0100

    create casoe specific branch

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -16,8 +16,13 @@ pip install -qr requirements.txt
 
 # Install and init Elastic Beanstalk Environment
 pip install -q awsebcli && echo "fvirt" > .ebignore &&
-eb init -k erepie -p python-2.7 --region eu-west-3 facile-erp && echo "Beanstalk environment initialized" ||
+eb init -k erepie -p python-2.7 --region eu-west-3 facile-demo && echo "Beanstalk environment initialized" ||
 echo "Beanstalk environment init failed"
+
+# It does not work now. Still, using eb configuration and changin line
+# static: facileapp/static
+# application_path: run.py
+# It works, yet we should manage to directly set a config file
 
 # Create application
 
