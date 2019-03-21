8a1ae1c7dcfac67fe31f14b1ac736fad127c35b4
erepie <pierre.mgouedard@gmail.com>
2019-03-21 11:26:38 +0100

    deployment debug

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -19,14 +19,16 @@ pip install -q awsebcli && echo "fvirt" > .ebignore &&
 eb init -k erepie -p python-2.7 --region eu-west-3 facile-erp && echo "Beanstalk environment initialized" ||
 echo "Beanstalk environment init failed"
 
-# TODO last 2 days:
-Create account for casoe OK
-Create super user in AWS's casoe account OK
-Create a database instance
-Make the ElasticBeansTalk routine for deployment (VERY EASY)
-Create secret-config on new machine
-Create File that gather every core information of application (aws web, etc)
+# It does not work now. Still, using eb configuration and changing line
+# static: facileapp/static/ (change using eb config)
+# application_path: run.py (change using eb config)
+# instance_type: t2.medium  option (eb create --instance_type t2.medium)
 
+# finally:
+# Connect to instance (ssh with username 'ubunutu' or 'ec2-user')
+# create secret-config file in /opt/python/current/app/secret-config.yaml
+
+# Create application
 
 
 
