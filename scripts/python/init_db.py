7add6e679e849fa8b19b384fc7c78eb902e7155f
erepie <pierre.mgouedard@gmail.com>
2019-03-21 17:39:32 +0100

    fix encoding bug local

--- scripts/python/init_db.py
+++ scripts/python/init_db.py
@@ -14,13 +14,14 @@ class Initializer():
         self.engine = db.create_engine(uri, encoding=CODEC)
 
     def create_database(self):
+
         # Remove old db an create new
         facile_base.metadata.drop_all(self.engine)
         facile_base.metadata.create_all(self.engine)
 
 
 if __name__ == '__main__':
-    init = Initializer('?'.join([d_sconfig['mysql_uri'], 'charset={}'.format(CODEC.replace('-', ''))]))
+    init = Initializer(d_sconfig['mysql_uri'])
     answer = raw_input("This action will drop every table, if you are sure enter: 'yesremoveallthis'")
     if answer == 'yesremoveallthis' or answer == "'yesremoveallthis'":
         init.create_database()
