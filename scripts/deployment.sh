33fdf4b1bdde77ec532a69d9aea075e91982dd61
erepie <pierre.mgouedard@gmail.com>
2019-05-29 14:45:13 +0200

    debug gitignore

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -19,16 +19,21 @@ pip install -q awsebcli && echo "fvirt" > .ebignore &&
 eb init -k erepie -p python-2.7 --region eu-west-3 facile-erp && echo "Beanstalk environment initialized" ||
 echo "Beanstalk environment init failed"
 
+
 # It does not work now. Still, using eb configuration and changing line
 # static: facileapp/static/ (change using eb config)
 # application_path: run.py (change using eb config)
 # instance_type: t2.medium  option (eb create --instance_type t2.medium)
 
-# finally:
-# Connect to instance (ssh with username 'ubunutu' or 'ec2-user')
-# create secret-config file in /opt/python/current/app/secret-config.yaml
+# If environement already exist GO directly here
+# If not sure about current environement
+eb list # list available environement
+eb use <environement_name>
+eb deploy
 
-# Create application
+# finally:
+# Connect to instance (ssh with username 'ubuntu' or 'ec2-user')
+# create secret-config file in /opt/python/current/app/secret-config.yml
 
 
 
